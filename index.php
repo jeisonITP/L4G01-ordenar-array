a6bec0351dcc3886737d50daeb3524d72a8558d3
Jeyson Calvache <jeison130@gmail.com>
2020-02-25 19:45:37 -0500

    Iniciando proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $array = [50,20,200,30,1,2];
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    Array de Entrada: <?= implode(',', $array) ?>
+    <br>
+    <?php sort($array) ?>
+    Array de Salida: <?= implode(',', $array)  ?>
+</body>
+</html>
