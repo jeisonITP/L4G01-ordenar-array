86620426474d37cb586cc72e88ff05cdb9dc7558
Jeyson Calvache <jeison130@gmail.com>
2020-02-25 19:59:07 -0500

    Cambiando titulo de la pagina

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba</title>
+    <title>Ordenerar Arrays</title>
 </head>
 <body>
     Array de Entrada: <?= implode(',', $array) ?>
